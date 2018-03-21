a4a66c7557d460e4b4a42d420aff3cb5d9051ba1
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-21 19:06:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,28 @@
-def display_board
-  board = [" "," "," "," "," "," "," "," "," "]
-  (rows[0]).to eq("   |   |   ")
-  (rows[1]).to eq("-----------")
-  (rows[2]).to eq("   |   |   ")
-  (rows[3]).to eq("-----------")
-  (rows[4]).to eq("   |   |   ")
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+
+
+
+
+
+
+
+
+
+# def display_board
+#   board = [" "," "," "," "," "," "," "," "," "]
+#   (rows[0]).to eq("   |   |   ")
+#   (rows[1]).to eq("-----------")
+#   (rows[2]).to eq("   |   |   ")
+#   (rows[3]).to eq("-----------")
+#   (rows[4]).to eq("   |   |   ")
+# end
 
 # def display_rainbow(color)
 #   puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
