16d2cc3134ef342d6d6ebe3d18d9e5df25e7ee2d
Asheley McDonald <asheleymcdonald@gmail.com>
2018-03-21 18:07:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
+def display_board
+  puts board = {" "," "," "," "," "," "," "," "," "}
+  {"rows[0]"}
+end
+
+# ```ruby
+# board = [" "," "," "," "," "," "," "," "," "]
+# display_board(board)
+
+
 # Define display_board that accepts a board and prints
 # out the current state.
